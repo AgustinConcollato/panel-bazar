ca4264d2ca548095d2691807bd0ac9e2fabd1ed3
Agustín Concollato <agustinconcollato@gmail.com>
2025-03-19 14:25:49 -0300

    eliminar archivos y estilos obsoletos de componentes y utilidades

--- src/utils/generateId.js
+++
@@ -1,9 +0,0 @@
-export function generateId() {
-    const a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    const id = []
-
-    for (let i = 0; i < 19; i++) {
-        id.push(a.charAt(Math.floor(Math.random() * a.length)))
-    }
-    return id.join('')
-}
