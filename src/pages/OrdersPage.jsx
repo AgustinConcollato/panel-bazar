401e464777a4ae677d2130238b5d2940b3414b9c
Agustín Concollato <agustinconcollato@gmail.com>
2025-03-19 14:27:19 -0300

    Agregar efecto para cambiar el título del documento en la página de pedidos

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react"
 import { CompletedOrders } from "../components/CompletedOrders/CompletedOrders"
 import { CreateOrder } from "../components/CreateOrder/CreateOrder"
 import { PendingsOrders } from "../components/PendingsOrders/PendingsOrders"
 
 export function OrderPage() {
+
+    useEffect(() => {
+        document.title = 'Pedidos'
+    }, [])
+
     return (
         <section className="order-page">
             <CreateOrder />
