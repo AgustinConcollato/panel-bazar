958abda1191391d6ddc334c06ef0a1d657819772
Agustín Concollato <agustinconcollato@gmail.com>
2025-03-25 19:21:48 -0300

    Refactor ProductList component: enhance CSS for layout and responsiveness, add product search functionality, and improve toast notifications for better user feedback

--- src/components/OrderComponents/ProductList/ProductList.jsx
+++ src/components/OrderComponents/ProductList/ProductList.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
+import { toast, ToastContainer } from 'react-toastify';
 import { api, urlStorage } from "../../../services/api";
 import { Loading } from "../../Loading/Loading";
-import { Pagination } from "../../Pagination/Pagination";
-import { toast, ToastContainer } from 'react-toastify'
-import './ProductList.css'
 import { Modal } from "../../Modal/Modal";
+import { Pagination } from "../../Pagination/Pagination";
+import './ProductList.css';
 
-export function ProductList({ orderId, setOrderProducts }) {
+export function ProductList({ orderId, setOrderProducts, setOrderData }) {
     const { Products, Order } = api;
     const products = new Products();
 
@@ -14,12 +14,14 @@ export function ProductList({ orderId, setOrderProducts }) {
     const [dataPage, setDataPage] = useState(null);
     const [productList, setProductList] = useState(null);
     const [selected, setSelected] = useState(false);
+    const [name, setName] = useState(null);
 
     async function getProducts() {
         const options = {
             page,
             category: null,
             status: 'active',
+            name
         };
 
         setProductList(null);
@@ -37,33 +39,76 @@ export function ProductList({ orderId, setOrderProducts }) {
 
         const { thumbnails, id, name, price, providers } = selected
 
-        console.log(selected)
+        const purchasePrice = () => {
+            if (providers.length == 0) {
+                return price / 2
+            } else if (providers.length == 1) {
+                return providers[0].pivot.purchase_price
+            } else {
+                const total = providers.reduce((a, provider) => a + parseFloat(provider.pivot.purchase_price), 0)
+                return total / providers.length
+            }
+        }
 
         thumbnails && formData.append('picture', thumbnails)
-        formData.append('purchase_price', providers.length != 0 ? providers[0].pivot.purchase_price : 0)
+        formData.append('purchase_price', purchasePrice())
         formData.append('product_id', id)
         formData.append('order_id', orderId)
         formData.append('name', name)
         formData.append('price', price)
 
         try {
-            const product = await orders.add(formData)
+            const product = await toast.promise(orders.add(formData), {
+                pending: 'Agregando producto...',
+                success: 'Se agregó correctamente',
+            })
 
             setOrderProducts(current => [...current, product])
+
+            setOrderData(current => ({
+                ...current,
+                total_amount: parseFloat(current.total_amount) + product.subtotal
+            }));
 
             setSelected(false)
 
         } catch (error) {
-            toast.error(error)
+            toast.error(error.error)
+            if (error.errors) {
+                return toast.error('Error, falta agregar la cantidad')
+            }
+
         }
     }
 
     useEffect(() => {
         getProducts();
-    }, [page]);
+    }, [page, name]);
+
+    useEffect(() => {
+        const handleKeyUp = (e) => {
+            if (e.keyCode == 27) {
+                setSelected(false)
+            }
+        }
+
+        document.addEventListener("keyup", handleKeyUp)
+
+        return () => {
+            document.removeEventListener("keyup", handleKeyUp)
+        }
+    }, [])
 
     return (
         <div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Buscar producto por nombre"
+                    className="input"
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
             {dataPage ? (
                 <>
                     <div className="list">
@@ -122,28 +167,14 @@ export function ProductList({ orderId, setOrderProducts }) {
                                 </div>
                             </div>
                             <div className="actions-edit">
-                                <button type="button" className="btn" onClick={() => setSelected(false)}>Cancelar</button>
                                 <button type="submit" className="btn btn-solid">Guardar</button>
+                                <button type="button" className="btn" onClick={() => setSelected(false)}>Cancelar</button>
                             </div>
                         </form>
                     </section>
                     <div className="background-modal" onClick={() => setSelected(false)}></div>
                 </Modal>
             }
-            <ToastContainer
-                position="top-right"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-                transition:Bounce
-                stacked
-            />
         </div>
     )
 }
