1e0f1c73b8ce97758f16fbdccba5f3d9eef638b9
Agustín Concollato <agustinconcollato@gmail.com>
2025-04-01 16:36:31 -0300

    Enhance ProductsPage component: implement product fetching with search parameters and update ProductList rendering

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -1,14 +1,41 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { ProductFilters } from "../../components/ProductComponents/ProductFilters/ProductFilters";
 import { ProductList } from "../../components/ProductComponents/ProductList/ProductList";
+import { Products } from "../../services/productsService";
 import './ProductsPage.css';
-import { useEffect } from "react";
 
 export function ProductsPage() {
 
+    const products = new Products();
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const [dataPage, setDataPage] = useState(null);
+    const [productList, setProductList] = useState(null);
+
+    async function getProducts() {
+        const options = {
+            page: searchParams.get("page") || 1,
+            category: searchParams.get("category") || null,
+            price: searchParams.get("price") || null,
+            status: searchParams.get("status") || null,
+            panel: true,
+        };
+
+        setProductList(null);
+        const dataPage = await products.search({ options });
+        setDataPage(dataPage);
+        setProductList(dataPage.data);
+
+    }
+
     useEffect(() => {
+        getProducts();
+
         document.title = 'Lista de productos'
-    }, [])
+
+    }, [searchParams]);
 
     return (
         <section className="products-page">
@@ -16,7 +43,7 @@ export function ProductsPage() {
                 <ProductFilters />
                 <Link to={'/agregar-producto'} className="btn btn-solid">+ Nuevo producto </Link>
             </div>
-            <ProductList />
+            <ProductList productList={productList} dataPage={dataPage} />
         </section>
     )
 }
