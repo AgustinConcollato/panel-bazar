ab1b42102dd16e8becbeb077316b84bb5d902dfb
Agustín Concollato <agustinconcollato@gmail.com>
2025-04-14 09:04:14 -0300

    Add OrderOptions component for order acceptance and rejection functionality

---
+++ src/components/OrderOptions/OrderOptions.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react"
+import { Modal } from "../../components/Modal/Modal"
+import { Order } from "../../services/ordersService"
+import './OrderOptions.css'
+import { useNavigate } from "react-router-dom"
+
+export function OrderOptions({ order, setOrders }) {
+
+    const orders = new Order()
+    const navigate = useNavigate()
+
+    const [modal, setModal] = useState(false)
+
+    async function rejectOrder() {
+        try {
+            const response = await orders.rejectOrder(order.id)
+
+            if (response) {
+                setModal(false)
+                setOrders(response)
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function acceptOrder() {
+        try {
+            const response = await orders.acceptOrder(order.id)
+
+            if (response) {
+                navigate(`/pedido/${order.id}`)
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <>
+            <div className="order-options container-btn">
+                <button
+                    className="btn btn-error-regular"
+                    onClick={() => setModal(true)}
+                >
+                    Rechazar pedido
+                </button>
+                <button
+                    className="btn btn-solid"
+                    onClick={acceptOrder}
+                >
+                    Aceptar pedido
+                </button>
+            </div>
+            {modal &&
+                <Modal>
+                    <div className="container-children">
+                        <h2>¿Estás seguro que deseas rechazar el pedido?</h2>
+                        <p>Una vez rechazado no podrás volver a aceptarlo</p>
+                        <div className="container-btn">
+                            <button
+                                className="btn"
+                                onClick={() => setModal(false)}
+                            >
+                                Cancelar
+                            </button>
+                            <button
+                                className="btn btn-error-solid"
+                                onClick={rejectOrder}
+                            >
+                                Rechazar
+                            </button>
+                        </div>
+                    </div>
+                    <div className="background-modal" onClick={() => setModal(false)}></div>
+                </Modal>
+            }
+        </>
+    )
+}
