41b920af230e9e9b7a05f810fdfef7232712549f
Agustín Concollato <agustinconcollato@gmail.com>
2025-04-14 09:03:29 -0300

    Add navigation and styling for order management in OrderPage

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react"
-import { CompletedOrders } from "../../components/CompletedOrders/CompletedOrders"
+import { Navigate, NavLink, Route, Routes } from "react-router-dom"
+import { AcceptedOrders } from "../../components/AcceptedOrders/AcceptedOrders"
+import { CompletedOrdersSummary } from "../../components/CompletedOrdersSummary/CompletedOrdersSummary"
 import { CreateOrder } from "../../components/CreateOrder/CreateOrder"
 import { PendingsOrders } from "../../components/PendingsOrders/PendingsOrders"
 import './OrderPage.css'
-import { CompletedOrdersSummary } from "../../components/CompletedOrdersSummary/CompletedOrdersSummary"
 
 export function OrderPage() {
 
@@ -17,8 +18,22 @@ export function OrderPage() {
                 <CreateOrder />
                 <CompletedOrdersSummary />
             </div>
-            <PendingsOrders />
-            {/* <CompletedOrders /> */}
+
+
+            <nav>
+                <NavLink to='' end className="link-orders">Pendientes</NavLink>
+                <NavLink to='aceptados' className="link-orders">Aceptados</NavLink>
+                {/* <NavLink to='rechazados' className="link-orders">Rechazados</NavLink> */}
+                {/* <NavLink to='cancelados' className="link-orders">Cancelados</NavLink> */}
+            </nav>
+            <Routes>
+                <Route index element={<PendingsOrders />} />
+                <Route path='pendientes' element={<PendingsOrders />} />
+                <Route path='aceptados' element={<AcceptedOrders />} />
+                {/* <Route path='rechazados' element={<RejectedOrders />} />
+                <Route path='cancelados' element={<CancelledOrders />} /> */}
+                <Route path="*" element={<Navigate to="/panel" replace />} />
+            </Routes>
         </section>
     )
 }
