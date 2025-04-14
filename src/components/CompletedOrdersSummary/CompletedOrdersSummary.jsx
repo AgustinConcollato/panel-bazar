a4fad12b1dc6170d958cbd0f1fed89609e7a73e0
Agustín Concollato <agustinconcollato@gmail.com>
2025-04-14 09:04:51 -0300

    Refactor CompletedOrdersSummary component to use custom hook for data fetching and simplify state management

--- src/components/CompletedOrdersSummary/CompletedOrdersSummary.jsx
+++ src/components/CompletedOrdersSummary/CompletedOrdersSummary.jsx
@@ -1,52 +1,24 @@
 import { Link } from 'react-router-dom';
-import './CompletedOrdersSummary.css'
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
+import { useCompletedOrders } from '../../hooks/useCompletedOrders';
 import { Loading } from '../Loading/Loading';
+import './CompletedOrdersSummary.css';
+import { useEffect, useState } from 'react';
 
 export function CompletedOrdersSummary() {
+
+
+    const [date, setDate] = useState(new Date());
+
+    const { orders, difference } = useCompletedOrders(date);
 
     const months = [
         'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
         'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
     ];
 
-    const date = new Date()
-    const { Order } = api
-
-    const [orders, setOrders] = useState(null);
-    const [difference, setDifference] = useState(null);
-
-    async function getOrders() {
-        const order = new Order();
-
-        const currentMonth = date.getMonth() + 1;
-        const lastMonth = currentMonth === 1 ? 12 : currentMonth - 1;
-
-        const currentYear = date.getFullYear();
-        const lastYear = currentMonth === 1 ? currentYear - 1 : currentYear;
-
-        try {
-            const currentOrders = await order.completed({ month: currentMonth, year: currentYear });
-
-            const lastOrders = await order.completed({ month: lastMonth, year: lastYear });
-
-            setOrders({ currentOrders, lastOrders });
-
-            const currentTotal = currentOrders.reduce((sum, order) => sum + parseFloat(order.total_amount), 0) || 0;
-            const lastTotal = lastOrders.reduce((sum, order) => sum + parseFloat(order.total_amount), 0) || 0;
-
-            const difference = lastTotal > 0 ? ((currentTotal - lastTotal) / lastTotal) * 100 : 0;
-
-            setDifference(difference)
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     useEffect(() => {
-        getOrders()
-    }, [])
+        setDate(new Date());
+    }, []);
 
     return (
         <div className='completed-orders-summary'>
@@ -65,7 +37,7 @@ export function CompletedOrdersSummary() {
                 </div> :
                 <Loading />
             }
-            <Link to={'/'} className="btn">Ver detalle del mes</Link>
+            <Link to={'/resumen-mensual'} className="btn">Ver detalle del mes</Link>
         </div>
     )
 }
