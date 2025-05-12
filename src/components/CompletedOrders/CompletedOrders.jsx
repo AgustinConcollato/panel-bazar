fcee7b405af8ed9bee65d984e2e48dccdeac1f07
Agustín Concollato <agustinconcollato@gmail.com>
2025-05-11 23:35:00 -0300

    Refactor CompletedOrders and CompletedOrdersSummary components for improved data handling
    - Updated CompletedOrders component to use 'orders.data' instead of 'orders.currentOrders'.
    - Modified CompletedOrdersSummary to utilize the new useCompareWithPreviousMonth hook for order comparison.
    - Adjusted rendering logic to display total order amount and comparison metrics accurately.

--- src/components/CompletedOrders/CompletedOrders.jsx
+++ src/components/CompletedOrders/CompletedOrders.jsx
@@ -4,19 +4,17 @@ import "./CompletedOrders.css";
 
 export function CompletedOrders({ orders }) {
     return (
-        <div className="container-orders">
-            {orders ? (
-                orders.currentOrders.length !== 0 ? (
-                    orders.currentOrders.map((e) => (
-                        <PreViewOrder key={e.id} order={e} />
-                    ))
-                ) : (
-                    <p>No hay pedidos terminados</p>
-                )
-            ) : (
-                <Loading />
-            )
-            }
-        </div >
+        <>
+            <div className="container-orders">
+                {orders ?
+                    orders.data.length !== 0 ?
+                        orders.data.map((e) => (
+                            <PreViewOrder key={e.id} order={e} />
+                        )) :
+                        <p>No hay pedidos terminados</p> :
+                    <Loading />
+                }
+            </div >
+        </>
     )
 }
--- src/components/CompletedOrdersSummary/CompletedOrdersSummary.jsx
+++ src/components/CompletedOrdersSummary/CompletedOrdersSummary.jsx
@@ -1,36 +1,31 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useCompletedOrders } from '../../hooks/useCompletedOrders';
+import { useCompareWithPreviousMonth } from '../../hooks/useCompareWithPreviousMonth';
 import { Loading } from '../Loading/Loading';
 import './CompletedOrdersSummary.css';
-import { useEffect, useState } from 'react';
 
 export function CompletedOrdersSummary() {
 
-
     const [date, setDate] = useState(new Date());
 
-    const { orders, difference } = useCompletedOrders(date);
+    const { orders, comparison } = useCompareWithPreviousMonth(date);
 
     const months = [
         'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
         'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
     ];
 
-    useEffect(() => {
-        setDate(new Date());
-    }, []);
-
     return (
         <div className='completed-orders-summary'>
-            {orders ?
+            {orders && comparison ?
                 <div>
                     <h3 className="title">Pedidos terminados en {months[date.getMonth()]}</h3>
-                    <p>Cantidad de pedidos <span>{orders.currentOrders.length}</span></p>
+                    <p>Cantidad de pedidos <span>{orders.quantity}</span></p>
                     <div>
-                        <h2>${parseInt(orders.currentOrders.reduce((a, order) => a + parseFloat(order.total_amount), 0)).toLocaleString()}</h2>
-                        <p className={difference < 0 ? 'inactive' : difference > 0 ? 'active' : 'diference'}>
-                            {difference > 0 ? '+' : ''}
-                            {difference.toFixed(2)}%
+                        <h2>${orders.total_amount}</h2>
+                        <p className={comparison.gross_profit_change < 0 ? 'inactive' : comparison.gross_profit_change > 0 ? 'active' : 'diference'}>
+                            {comparison.gross_profit_change > 0 ? '+' : ''}
+                            {comparison.gross_profit_change.toFixed(2)}%
                             <span>En relación al mes pasado</span>
                         </p>
                     </div>
