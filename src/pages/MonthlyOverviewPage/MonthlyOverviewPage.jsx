fdfcd38c66e67ad0e1b6fee0c6613f819ef898d9
Agustín Concollato <agustinconcollato@gmail.com>
2025-05-11 23:35:46 -0300

    Refactor MonthlyOverviewPage to integrate new analytics and comparison features
    - Replaced useCompletedOrders and useNetProfit hooks with useCompareWithPreviousMonth for enhanced monthly order comparison.
    - Introduced analytics service to fetch monthly resume data.
    - Updated rendering logic to display comparison metrics and order details based on the new data structure.
    - Added pagination support and improved state management for better user experience.

--- src/pages/MonthlyOverviewPage/MonthlyOverviewPage.jsx
+++ src/pages/MonthlyOverviewPage/MonthlyOverviewPage.jsx
@@ -2,41 +2,38 @@ import { useEffect, useState } from "react";
 import { CompletedOrderFilter } from "../../components/CompletedOrderFilter/CompletedOrderFilter";
 import { CompletedOrders } from "../../components/CompletedOrders/CompletedOrders";
 import { Loading } from "../../components/Loading/Loading";
-import { useCompletedOrders } from "../../hooks/useCompletedOrders";
-import { useNetProfit } from "../../hooks/useNetProfit";
+import { useCompareWithPreviousMonth } from "../../hooks/useCompareWithPreviousMonth";
 import './MonthlyOverviewPage.css';
+import { Pagination } from "../../components/Pagination/Pagination";
+import { Analytics } from "../../services/analyticsService";
+import { OrderList } from "../../components/OrderList/OrderList";
 
 export function MonthlyOverviewPage() {
 
     const currentDate = new Date();
+    const analytics = new Analytics();
     const [date, setDate] = useState(new Date(currentDate.getFullYear(), currentDate.getMonth(), 1));
     const [monthFilter, setMonthFilter] = useState(date.getMonth() + 1); // Estado para el mes seleccionado
 
-    const { orders, difference } = useCompletedOrders(date);
-    const { netProfit } = useNetProfit(date);
     const currentYear = currentDate.getFullYear();
     const startYear = 2025;
     const years = Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i);
 
-    let grossProfit = 0;
-    let profit = 0;
-    let profitPercentage = 0;
-    let cost = 0;
+    const { orders, comparison } = useCompareWithPreviousMonth(date);
 
-    if (orders && netProfit) {
-        grossProfit = parseFloat(
-            orders.currentOrders.reduce((a, order) => a + parseFloat(order.total_amount), 0)
-        );
+    const [page, setPage] = useState(1);
+    const [resume, setResume] = useState(null);
 
-        profit = parseFloat(netProfit);
-        cost = grossProfit - profit;
-        profitPercentage = grossProfit > 0 ? (profit / grossProfit) * 100 : 0;
-    }
     const months = [
         'Todos los meses',
         'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
         'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
     ];
+
+    async function getResume() {
+        const response = await analytics.resume({ year: date.getFullYear(), month: date.getMonth() + 1 });
+        setResume(response.resume);
+    }
 
     function handleMonthChange(e) {
         const selectedValue = e.target.value;
@@ -57,6 +54,7 @@ export function MonthlyOverviewPage() {
     }
 
     useEffect(() => {
+        getResume();
         document.title = 'Resumen de ' + months[date.getMonth() + 1];
     }, [date]);
 
@@ -71,39 +69,41 @@ export function MonthlyOverviewPage() {
                     handleMonthChange={handleMonthChange}
                     handleYearChange={handleYearChange}
                 />
-                <h1>Detalle de {months[date.getMonth() + 1]}
-                    <p className={difference < 0 ? 'inactive' : difference > 0 ? 'active' : 'diference'}>
-                        {difference > 0 ? '+' : ''}
-                        {difference?.toFixed(2)}%
-                        <span>En relación al mes pasado</span>
-                    </p>
-                </h1>
-                {(orders && netProfit) ?
-                    <div>
-                        <p>Cantidad de pedidos <span>{orders.currentOrders.length}</span></p>
-                        <div className="profit">
-                            <h2>
-                                <span>Granancia bruta</span>
-                                ${grossProfit.toFixed(2)}
-                            </h2>
-                            <h2>
-                                <span>Granancia neta</span>
-                                ${profit.toFixed(2)}
-                            </h2>
-                            <h2>
-                                <span>Porcentaje de ganancia</span>
-                                {profitPercentage.toFixed(2)}%
-                            </h2>
-                            <h2>
-                                <span>Costos</span>
-                                ${cost.toFixed(2)}
-                            </h2>
+                {resume && comparison ?
+                    <>
+                        <h1>Detalle de {months[date.getMonth() + 1]}
+                            <p className={comparison.gross_profit_change < 0 ? 'inactive' : comparison.gross_profit_change > 0 ? 'active' : 'diference'}>
+                                {comparison.gross_profit_change > 0 ? '+' : ''}
+                                {comparison.gross_profit_change.toFixed(2)}%
+                                <span>En relación al mes pasado</span>
+                            </p>
+                        </h1>
+                        <div>
+                            <p>Cantidad de pedidos <span>{orders.quantity}</span></p>
+                            <div className="profit">
+                                <h2>
+                                    <span>Granancia bruta</span>
+                                    ${resume.gross_profit.toFixed(2)}
+                                </h2>
+                                <h2>
+                                    <span>Granancia neta</span>
+                                    ${resume.net_profit.toFixed(2)}
+                                </h2>
+                                <h2>
+                                    <span>Porcentaje de ganancia</span>
+                                    {resume.profit_percentage.toFixed(2)}%
+                                </h2>
+                                <h2>
+                                    <span>Costos</span>
+                                    ${resume.cost.toFixed(2)}
+                                </h2>
+                            </div>
                         </div>
-                    </div> :
+                        <OrderList status="completed" year={date.getFullYear()} month={date.getMonth() + 1} />
+                    </> :
                     <Loading />
                 }
             </div>
-            <CompletedOrders orders={orders} />
         </div>
     )
 } 
