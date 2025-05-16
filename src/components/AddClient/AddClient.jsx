cff9ae12d8a11bdfe018c1a3b343d6af50671e65
Agustin <agustinconcollato@gmail.com>
2025-05-16 01:29:11 -0300

    Update AddClient component to simplify response handling
    - Changed the condition for successful response from checking status to a direct truthy check on the response object.
    - This adjustment improves the clarity and reliability of the form reset logic after adding a client.

--- src/components/AddClient/AddClient.jsx
+++ src/components/AddClient/AddClient.jsx
@@ -24,7 +24,7 @@ export function AddClient() {
                 error: 'Error, no se puedo agregar'
             })
 
-            if (response.status == 'success') {
+            if (response) {
                 e.target.reset()
             }
 
