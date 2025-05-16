1d18520da9592f9b53fcdc83a84cf296cd1269b5
Agustin <agustinconcollato@gmail.com>
2025-05-16 01:33:39 -0300

    Add ClientsPage component and update import path
    - Introduced the ClientsPage component to fetch and display a list of clients, including loading and empty state handling.
    - Updated the import path for ClientsPage in App.jsx to reflect the new file structure.

--- src/pages/ClientsPage/ClientsPage.jsx
+++ src/pages/ClientsPage/ClientsPage.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
-import { Loading } from "../components/Loading/Loading"
+import { Loading } from "../../components/Loading/Loading"
 import { Link } from "react-router-dom"
-import { ClientList } from "../components/ClientList/ClientList"
-import { api } from "../services/api"
+import { ClientList } from "../../components/ClientList/ClientList"
+import { api } from "../../services/api"
 
 export function ClientsPage() {
 
