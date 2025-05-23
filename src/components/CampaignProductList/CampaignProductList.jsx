ce1c31f577405bb27e44a6debbaa592bc9997424
Agustín Concollato <agustinconcollato@gmail.com>
2025-05-23 16:12:48 -0300

    Add CampaignProductList component with product selection functionality
    - Introduced the CampaignProductList component to manage and display products associated with a campaign, including search and selection capabilities.
    - Implemented state management for selected products and loading states, enhancing user interaction during product searches.
    - Added CSS styles for the CampaignProductList component to ensure a visually appealing layout and improved user experience.

---
+++ src/components/CampaignProductList/CampaignProductList.jsx
@@ -0,0 +1,139 @@
+import { faXmark } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
+import { urlStorage } from "../../services/api"
+import { Campaign } from "../../services/campaignServices"
+import { Products } from "../../services/productsService"
+import { CampaignProduct } from "../CampaignProduct/CampaignProduct"
+import { Loading } from "../Loading/Loading"
+import './CampaignProductList.css'
+
+export function CampaignProductList({ campaign }) {
+
+    const [selectedProducts, setSelectedProducts] = useState([])
+    const [products, setProducts] = useState(campaign.products)
+    const [results, setResults] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    async function search(e) {
+        const name = e.target.value.trim()
+
+        if (name.length == 0) {
+            setResults([])
+            return
+        }
+
+        setLoading(true)
+        try {
+            const response = await new Products().search({ options: { name, page: 1, panel: true } })
+            const filteredResults = response.data.map(product => {
+                const isInCampaign = products.some(campaignProduct => campaignProduct.id === product.id)
+                return {
+                    ...product,
+                    in_campaign: isInCampaign
+                }
+            })
+            setResults(filteredResults)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    async function addProductsToCampaign() {
+        try {
+            const response = await new Campaign().addProducts({ products: selectedProducts, campaignId: campaign.id })
+            setProducts(response.products)
+            setSelectedProducts([])
+            setResults(null)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function deleteProductsToCampaign(productId) {
+        try {
+            const response = await new Campaign().deleteProduct({ productId, campaignId: campaign.id })
+            setProducts(currentProducts => currentProducts.filter(product => product.id !== response.product_id))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    function selectProduct(e) {
+
+        const { id, name } = e
+
+        const product = {
+            campaign_id: campaign.id,
+            product_id: id,
+            custom_discount_type: campaign.discount_type,
+            custom_discount_value: campaign.discount_value,
+            name
+        }
+
+        setSelectedProducts(current => [...current, product])
+    }
+
+    return (
+        <div className="campaign-products">
+            <div>
+                <input
+                    type="search"
+                    placeholder="Buscar productos"
+                    className="input"
+                    onChange={search}
+                />
+            </div>
+            {!loading ?
+                results && results.length > 0 ?
+                    <div className="results">
+                        {results.map(e => {
+                            const isSelected = selectedProducts.find(p => p.product_id === e.id)
+                            return (
+                                <div
+                                    key={e.id}
+                                    className={`product ${e.in_campaign ? 'product-in-campaign' : ''} ${isSelected ? 'product-select' : ''}`}
+                                    onClick={() => !isSelected && !e.in_campaign && selectProduct(e)}
+                                >
+                                    <div className="container-img">
+                                        <img src={urlStorage + '/' + JSON.parse(e.thumbnails)[0]} />
+                                    </div>
+                                    <h4>{e.name}</h4>
+                                </div>
+                            )
+                        }
+                        )}
+                    </div> :
+                    <p>No hay resultados</p> :
+                <p><Loading /></p>
+            }
+            {selectedProducts.length > 0 &&
+                <div>
+                    <h3>Seleccionados</h3>
+                    <ul>
+                        {selectedProducts.map(e =>
+                            <li>
+                                <span>{e.name}</span>
+                                <FontAwesomeIcon icon={faXmark} onClick={() => setSelectedProducts(current => current.filter(f => f.product_id != e.product_id))} />
+                            </li>
+                        )}
+                    </ul>
+                    <button className="btn btn-solid" onClick={addProductsToCampaign}>Agregar seleccionados</button>
+                </div>
+            }
+            <div>
+                <h3>Productos que ya estan en "{campaign.name}"</h3>
+                {products.length > 0 ?
+                    <div className="results">
+                        {products.map(e => <CampaignProduct e={e} campaignId={campaign.id} onDelete={deleteProductsToCampaign} />)}
+                    </div> :
+                    <div className="results">
+                        <p>No hay productos</p>
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
