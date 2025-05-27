0a929e56fe456f827966c07c2a8eef3ad1b08d00
Agustín Concollato <agustinconcollato@gmail.com>
2025-05-27 00:25:06 -0300

    Add MovementsList component with styling and functionality
    - Introduced the MovementsList component to display grouped financial movements by date, enhancing the user interface for transaction management.
    - Added corresponding CSS styles for layout, including movement grouping, icons, and responsive design for better visual clarity.

---
+++ src/components/MovementsList/MovementsList.jsx
@@ -0,0 +1,56 @@
+import { MoneyReceiveIcon, MoneySendIcon } from "../../icons/icons"
+import './MovementsList.css'
+
+export function MovementsList({ movements }) {
+    function groupMovementsByDate(movements) {
+        return movements.reduce((groups, movement) => {
+            const date = new Date(movement.updated_at).toLocaleDateString()
+            if (!groups[date]) {
+                groups[date] = []
+            }
+            groups[date].push(movement)
+            return groups
+        }, {})
+    }
+
+    return (
+        <>
+            <h3>Últimos movimientos</h3>
+            <div className="movements-container">
+                {Object.entries(groupMovementsByDate(movements)).map(([date, dateMovements]) => (
+                    <div key={date} className="movement-group">
+                        <h4 className="movement-date-header">{date}</h4>
+                        <ul className="movements-list">
+                            {dateMovements.map((e, index) => (
+                                <li key={index} className={`movement ${e.type}`}>
+                                    <div className="movement-info">
+                                        <div className="movement-info-icon">
+                                            {e.type === "in" ?
+                                                <MoneyReceiveIcon color={'#000'} height={'30px'} width={'30px'} /> :
+                                                <MoneySendIcon color={'#000'} height={'30px'} width={'30px'} />
+                                            }
+                                        </div>
+                                        <div>
+                                            <span className="movement-type">
+                                                {e.type === "in" ? "Ingreso" : "Egreso"}
+                                            </span>
+                                            <span className="movement-description">{e.description}</span>
+                                        </div>
+                                    </div>
+                                    <div className="movement-details">
+                                        <span className="movement-amount movement-type">
+                                            {e.type === "in" ? '+' : '-'} ${parseFloat(e.amount).toLocaleString('es-AR', { maximumFractionDigits: 2 })}
+                                        </span>
+                                        <span className="movement-time">
+                                            {new Date(e.updated_at).toLocaleTimeString('es-AR', { hour: '2-digit', minute: '2-digit', hour12: false })} hs
+                                        </span>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+} 
