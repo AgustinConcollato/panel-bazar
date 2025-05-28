75d59a64e10b13ad4b4c528f86d6a17554e6382c
Agustin <agustinconcollato@gmail.com>
2025-05-28 12:41:09 -0300

    Enhance PaymentOption component to include paid amount input and buttons for quick payment options

--- src/components/PaymentOption/PaymentOption.jsx
+++ src/components/PaymentOption/PaymentOption.jsx
@@ -4,8 +4,8 @@ import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import './PaymentOption.css';
 
 export function PaymentOption({ createPay, order, loading }) {
-
     const [selectedMethod, setSelectedMethod] = useState(null);
+    const [paidAmount, setPaidAmount] = useState();
 
     const paymentOptions = ['transfer', 'cash', 'check'];
 
@@ -23,13 +23,13 @@ export function PaymentOption({ createPay, order, loading }) {
 
     function addMethodAndConfirmPay() {
         const now = new Date();
-        const formattedDate = now.toISOString().slice(0, 19).replace('T', ' '); // Formato: YYYY-MM-DD HH:mm:ss
+        const formattedDate = now.toISOString().slice(0, 19).replace('T', ' ');
 
         const data = {
             order_id: order.id,
             method: selectedMethod,
             expected_amount: order.total_amount,
-            paid_amount: order.total_amount,
+            paid_amount: parseFloat(paidAmount),
             paid_at: formattedDate
         }
 
@@ -54,15 +54,37 @@ export function PaymentOption({ createPay, order, loading }) {
                     </div>
                 ))}
             </div>
-            {selectedMethod &&
-                <div className="container-btn">
-                    <button className='btn btn-solid' onClick={addMethodAndConfirmPay} disabled={loading}>
-                        {loading ? <FontAwesomeIcon icon={faCircleNotch} spin /> : 'Agregar y confirmar pago'}
-                    </button>
-                    <button className='btn btn-regular' onClick={addMethod} disabled={loading}>
-                        {loading ? <FontAwesomeIcon icon={faCircleNotch} spin /> : 'Agregar método'}
-                    </button>
-                </div>}
+            {selectedMethod && (
+                <>
+                    <div className="amount-input">
+                        <p>Monto a pagar de {order.total_amount}</p>
+                        <div>
+                            <input
+                                className='input'
+                                placeholder='Monto a pagar'
+                                type="number"
+                                value={paidAmount}
+                                onChange={(e) => setPaidAmount(e.target.value)}
+                                min="0"
+                                max={order.total_amount}
+                                step="0.01"
+                            />
+                            <div className="container-btn">
+                                <button onClick={()=> setPaidAmount(order.total_amount / 2)}>Mitad del monto total</button>
+                                <button onClick={()=> setPaidAmount(order.total_amount)}>Monto total</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-btn">
+                        <button className='btn btn-solid' onClick={addMethodAndConfirmPay} disabled={loading}>
+                            {loading ? <FontAwesomeIcon icon={faCircleNotch} spin /> : 'Agregar y confirmar pago'}
+                        </button>
+                        <button className='btn btn-regular' onClick={addMethod} disabled={loading}>
+                            {loading ? <FontAwesomeIcon icon={faCircleNotch} spin /> : 'Agregar método'}
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
