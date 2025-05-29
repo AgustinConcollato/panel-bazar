b6658fb4d6e9c4fa1e34b51a8d9cda02cda5cd6a
Agustin <agustinconcollato@gmail.com>
2025-05-29 15:43:12 -0300

    Add ClientDetail route to App component for detailed client view

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { AddProvider } from './components/AddProvider/AddProvider'
 import { Loading } from './components/Loading/Loading'
 import { NavBar } from './components/NavBar/NavBar'
 import { Order } from './components/OrderComponents/Order/Order'
+import { ClientDetail } from './components/ClientDetail/ClientDetail'
 import { ProductDetail } from './components/Product/ProductDetail/ProductDetail'
 import { Search } from './components/Search/Search'
 import { AuthContext } from './context/AuthContext'
@@ -49,6 +50,7 @@ function App() {
                 <Route path='/pedidos/*' element={<OrderPage />} />
                 <Route path='/pedido/:id/*' element={<Order />} />
                 <Route path='/clientes' element={<ClientsPage />} />
+                <Route path='/cliente/:id' element={<ClientDetail />} />
                 <Route path='/proveedores/*' element={<ProviderPage />} />
                 <Route path='/agregar-proveedor' element={<AddProvider />} />
                 <Route path='/resumen-mensual' element={<MonthlyOverviewPage />} />
