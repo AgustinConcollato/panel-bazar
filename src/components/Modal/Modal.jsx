1de35a80c25f647e676f191fdaa65ba2bafad882
Agustin <agustinconcollato@gmail.com>
2025-06-10 15:49:42 -0300

    Refactor Modal component to manage focus on the first input element and update z-index for better layering

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,22 @@
 import { createPortal } from 'react-dom'
+import { useEffect, useRef } from 'react'
 import './Modal.css'
 
 export function Modal({ children }) {
+    const modalRef = useRef(null)
+
+    useEffect(() => {
+        if (modalRef.current) {
+            // Busca el primer input dentro del modal y le da focus
+            const firstInput = modalRef.current.querySelector('input, textarea, select, [tabindex]:not([tabindex="-1"])');
+            if (firstInput) {
+                firstInput.focus();
+            }
+        }
+    }, [])
+
     return createPortal(
-        <div className='modal'>{children}</div>,
+        <div className='modal' ref={modalRef}>{children}</div>,
         document.getElementById("root")
-      )
+    )
 }
